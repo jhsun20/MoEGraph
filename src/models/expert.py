29662715356b840af5e9f0eed3840f4e575fc273
jhsun20 <jhsun163@gmail.com>
2025-08-16 03:26:32 -0400

    updated experts

--- src/models/expert.py
+++ src/models/expert.py
@@ -390,11 +390,11 @@ class Experts(nn.Module):
         if self.training:
             u = torch.rand_like(logits)
             g = -torch.log(-torch.log(u + 1e-20) + 1e-20)
-            y_soft = torch.sigmoid((logits + g) / 0.1)
+            y_soft = torch.sigmoid((logits + g) / T)
         else:
             u = torch.rand_like(logits)
             g = -torch.log(-torch.log(u + 1e-20) + 1e-20)
-            y_soft = torch.sigmoid((logits + g) / 0.1)
+            y_soft = torch.sigmoid((logits + g) / T)
 
         y_hard = (y_soft > 0.5).float()
         return y_hard + (y_soft - y_soft.detach())
