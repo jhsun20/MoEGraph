1eb74dda4cccfd10a3968b1908f82895eb48adb8
jhsun20 <jhsun163@gmail.com>
2025-08-17 20:26:16 -0400

    updated experts

--- src/run_all.sh
+++ src/run_all.sh
@@ -2,13 +2,7 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_zinc_scaffold.yaml"
-  "config/config_zinc_size.yaml"
-  "config/config_cmnist.yaml"
   "config/config_sst2.yaml"
-  "config/config_twitter.yaml"
-  "config/config_motif_basis.yaml"
-  "config/config_motif_size.yaml"
   "config/config_hiv_scaffold.yaml"
   "config/config_hiv_size.yaml"
 )
