cdb077501bccac84a2222f9af3d22c3886f0c4f6
jhsun20 <jhsun163@gmail.com>
2025-08-20 10:07:56 -0400

    updated experts

--- src/run_all.sh
+++ src/run_all.sh
@@ -2,9 +2,9 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_hiv_scaffold.yaml"
   "config/config_motif_size.yaml"
   "config/config_cmnist.yaml"
+  "config/config_motif_basis.yaml"
 )
 
 # Run each config sequentially
