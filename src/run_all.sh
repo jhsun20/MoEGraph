cfcc90bb15e54604369e2a870f57cca4d645d197
jhsun20 <jhsun163@gmail.com>
2025-08-22 00:53:56 -0400

    updated experts

--- src/run_all.sh
+++ src/run_all.sh
@@ -2,8 +2,8 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_twitter.yaml"
   "config/config_sst2.yaml"
+  "config/config_twitter.yaml"
   "config/config_hiv_scaffold.yaml"
   "config/config_motif_basis.yaml"
 )
