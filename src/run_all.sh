397c70ee4aadc4264dbd04a2f9ff055cacf24ce9
jhsun20 <jhsun163@gmail.com>
2025-08-25 02:38:55 -0400

    updated experts

--- src/run_all.sh
+++ src/run_all.sh
@@ -2,9 +2,10 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_motif_size.yaml"
-  "config/config_motif_basis.yaml"
+  "config/config_hiv_size.yaml"
   "config/config_cmnist.yaml"
+  "config/config_sst2.yaml"
+  "config/config_twitter.yaml"
 )
 
 # Run each config sequentially
