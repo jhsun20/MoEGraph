43672de50ec7361576492ae27086e6b428a61a72
jhsun20 <jhsun163@gmail.com>
2025-08-30 18:41:20 -0400

    updated experts

--- src/models/expert.py
+++ src/models/expert.py
@@ -345,7 +345,7 @@ class Experts(nn.Module):
                 edge_mask_k = edge_masks[:, k, :]
 
                 # ----- CE (per-sample + scalar) -----
-                if self.metric == "Accuracy":
+                if self.metric != "MAE":
                     ce_vec = F.cross_entropy(logits_k, target.view(-1).long(), reduction='none') * self.weight_ce  # (B,)
                 else:
                     pred = logits_k.squeeze(-1).float()
