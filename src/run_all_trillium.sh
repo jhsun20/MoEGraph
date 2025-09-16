a162f663165363c05afd25344182b2acfd4f15a4
jhsun20 <jhsun163@gmail.com>
2025-09-16 01:35:11 -0400

    updated experts

--- src/run_all_trillium.sh
+++ src/run_all_trillium.sh
@@ -2,9 +2,7 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_twitter.yaml"
   "config/config_motif_size.yaml"
-  "config/config_motif_basis.yaml"
   "config/config_sst2.yaml"
 )
 
