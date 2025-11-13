1e7ed9ed47940aaad79b66bd24a4705afc834878
jhsun20 <jhsun163@gmail.com>
2025-11-13 04:32:30 -0500

    updated experts

--- src/run_all_trillium.sh
+++ src/run_all_trillium.sh
@@ -2,9 +2,9 @@
 
 # List of config files to run (edit this list)
 CONFIGS=(
-  "config/config_motif2_basis.yaml"
-  "config/config_hiv_size.yaml"
-  "config/config_hiv_scaffold.yaml"
+  "config/config_motif2_basis_covariate.yaml"
+  "config/config_motif2_basis_FIIF.yaml"
+  "config/config_motif2_basis_PIIF.yaml"
 )
 
 # Run each config sequentially
